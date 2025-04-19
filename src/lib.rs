477400d881ee4980352b5ff5246ac633297a12f8
John Reagan <john@ramplingreagans.com>
2025-04-19 03:47:25 -0700

    Imported the modules from the old  lib_jt  library.

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@
                             lib_utils
 
                 Miscellaneous utility functions
-*/
 
-pub const _VERSION: &str = "0.0.5";
-pub const _AUTHOR: &str = "John T. Reagan";
-pub const _LICENSE: &str = "MIT";
-pub const _LICENSE_URL: &str = "https://opensource.org/licenses/MIT";
-pub const _REPOSITORY: &str = "https://github.com/jtreagan/lib_utils";
-pub const _COPYRIGHT: &str = "Copyright (c) 2025 John T. Reagan";
+VERSION = "0.0.5";
+AUTHOR = "John T. Reagan";
+LICENSE = "MIT";
+LICENSE_URL = "https://opensource.org/licenses/MIT";
+REPOSITORY = "https://github.com/jtreagan/lib_utils";
+COPYRIGHT = "Copyright (c) 2025 John T. Reagan";
+
+*/  // Credits
 
 
 pub mod utilities {
@@ -149,3 +150,229 @@ pub mod utilities {
      */  // util_clear_terminal()  &  divide_string_tovec()  functions.
 
 } // End utilities module.
+
+pub mod misc {
+
+    use dialoguer::Select;
+
+    /* ~~~~~~~~~~~~~~  Menu Function Documentaion  ~~~~~~~~~~~~~~~
+
+----------------------------------
+Requires the following in the Cargo.toml file:
+
+[dependencies]
+dialoguer = { version = "0.11.0", features = ["completion"] }
+console = "0.15.8"
+
+----------------------------------
+
+Example:
+
+fn main() {
+
+let opts: Vec<String> = vec!["Activity 1".to_string(),
+            "Activity 2".to_string(),
+            "Activity 3".to_string(),
+            "Activity 4".to_string(),
+            "Activity 5".to_string(),
+            "Activity 6".to_string(),
+            "Activity 7".to_string(),];
+
+let choice = activity_menu(&opts, "This is a prompt);
+
+println!("\n You chose choice # {}. \n", choice );
+}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */  // Example for using  activity_menu()  function.
+
+    pub fn activity_menu(opts: &Vec<String>, prompt: &str) -> usize {
+        // For a terminal-based menu this works pretty good.  The  opts
+        //      vector contains the menu choices.
+        let choice: usize;
+
+        let index = Select::new()
+            .with_prompt(prompt)
+            .report(false)
+            .items(opts)
+            .default(0)
+            .interact();
+
+        match index {
+            Ok(index) => { choice = index + 1; },
+            Err(error) => panic!("Bad return from activity menu:  {}.", error),
+        };
+        choice
+    }
+} // End of misc module
+
+pub mod input_utilities {
+    // Note that these are all terminal-based functions that don't
+    //      require a GUI.
+
+    use std::io;
+    use std::io::Write;
+    use std::str::FromStr;
+
+    pub fn input_num_prompt_range(prompt: &str, min: i64, max: i64) -> i64 {
+        // Changed in choice_menu.rs
+        // use std::io::{self, Write};
+
+        loop {
+            print!("{}", prompt);
+            let _ = io::stdout().flush().expect("Failed to flush stdout.");
+
+            let mut input = String::new();
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read input.");
+
+            let input = match input.trim().parse() {
+                Ok(parsed_input) => parsed_input,
+                Err(_) => continue,
+            };
+
+            if (min..=max).contains(&input) {
+                return input;
+            } else {
+                println!("\n That choice isn't allowed!  \n");
+                continue;
+            };
+        }
+    }
+
+    pub fn input_num_prompt<U: FromStr>(prompt: &str) -> U {
+        //use std::io::{self, Write};
+
+        loop {
+            print!("{}", prompt);
+            let _ = io::stdout().flush().expect("Failed to flush stdout.");
+
+            let mut input = String::new();
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read input.");
+
+            let input = match input.trim().parse::<U>() {
+                Ok(parsed_input) => parsed_input,
+                Err(_) => continue,
+            };
+
+            return input;
+        }
+    }
+
+    pub fn input_string_prompt(prompt: &str) -> String {
+        //use std::io::{self, Write};
+
+        print!("{}", prompt);
+        let _ = io::stdout().flush().expect("Failed to flush stdout.");
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input.trim().to_string()
+    }
+
+    pub fn input_char_prompt(message: &str) -> char {
+        let word = input_string_prompt(message);
+        let mut letter = word.chars();
+        let character = letter.next().unwrap();
+
+        character
+    }
+
+    pub fn input_bool_prompt(message: &str) -> bool {
+        let mut value: String;
+        let truth_value: bool;
+
+        loop {
+            value = input_string_prompt(message).to_uppercase();
+            match value.as_str() {
+                "YES" => {
+                    truth_value = true;
+                    break;
+                }
+                "Y" => {
+                    truth_value = true;
+                    break;
+                }
+                "NO" => {
+                    truth_value = false;
+                    break;
+                }
+                "N" => {
+                    truth_value = false;
+                    break;
+                }
+                "TRUE" => {
+                    truth_value = true;
+                    break;
+                }
+                "T" => {
+                    truth_value = true;
+                    break;
+                }
+                "FALSE" => {
+                    truth_value = false;
+                    break;
+                }
+                "F" => {
+                    truth_value = false;
+                    break;
+                }
+                _ => {
+                    println!(
+                        "Please enter one of  yes, Yes, y, Y, no, No, n, N, true, t,
+            false, f, True, T, False, or F"
+                    );
+                }
+            };
+        }
+        truth_value
+    }
+
+} // End input_utilities module
+
+pub mod vec {
+    use rand::Rng;
+
+    pub fn vec_random_choice<T>(list: &Vec<T>) -> Option<(&T, usize)> {
+        if list.is_empty() {
+            return None;
+        }
+
+        let index = rand::thread_rng().gen_range(0..list.len());
+        Some((&list[index], index))
+    }
+
+    pub fn vec_string_to_str(list: &Vec<String>) -> Vec<&str> {
+        let mut uselist: Vec<&str> = Vec::new();
+        let mut i = 0;
+
+        while i < list.len() {
+            uselist.push(&list[i].as_str());
+            i += 1;
+        }
+
+        uselist
+    }
+
+    pub fn vec_longest_str(v: &Vec<String>) -> Option<&String> {
+        v.iter().max_by_key(|x| x.len())
+    }
+
+    /* ---------------------  Example for using vec_longest_str()
+        fn main() {
+            let words = vec!["a limerick".to_string(), "vall leaves".to_string(), "snow".to_string(), "flowers blue".to_string(), "A really nice sunset".to_string()];
+            let longest = vec_longest_str(&words);
+
+            match longest {
+                Some(word) => println!("\nThe longest word is: {}/n", word),
+                None => println!("The vector is empty."),
+            }
+        }
+
+     */  //Example for using vec_longest_str()
+
+} // End vec module
